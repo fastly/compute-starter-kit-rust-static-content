c0bb19ed5bccb23ef0a0eefcfaa0ed3e5b34329f
Kailan Blanks <kblanks@fastly.com>
2020-11-24 18:32:24 +0000

    Directories work as expected

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Compute@Edge static content starter kit program.
 
-use fastly::http::{StatusCode};
+use fastly::http::StatusCode;
 use fastly::{Error, Request, Response};
 
 /// The name of a backend server associated with this service.
@@ -20,29 +20,38 @@ const BUCKET_NAME: &str = "betts-gcp-gcs-fastly-tutorial";
 ///
 /// If `main` returns an error, a 500 error response will be delivered to the client.
 #[fastly::main]
-fn main(req: Request) -> Result<Response, Error> {
+fn main(mut req: Request) -> Result<Response, Error> {
   let original_path = req.get_path();
 
   let path = if original_path.ends_with('/') {
     format!("/{}{}index.html", BUCKET_NAME, original_path)
   } else {
-    req.get_path().to_string()
+    format!("/{}{}", BUCKET_NAME, original_path)
   };
 
-  let mut bereq = req.with_path(&path).with_header("Host", BUCKET_HOST);
+  req.set_path(&path);
+  req.set_header("Host", BUCKET_HOST);
 
-  // Try to fetch
+  let mut bereq = copy_request(&req);
   let mut beresp = bereq.send(BACKEND_NAME)?;
 
-  if beresp.get_status() == StatusCode::NOT_FOUND && path.ends_with("/") {
-    bereq.set_path(&format!("{}/index.html", path));
+  if beresp.get_status() == StatusCode::NOT_FOUND {
+    bereq = copy_request(&req);
+    bereq.set_path(&format!("{}/index.html", bereq.get_path()));
     beresp = bereq.send(BACKEND_NAME)?;
   }
 
   if beresp.get_status() == StatusCode::NOT_FOUND {
-    bereq.set_path("/404.html");
+    bereq = copy_request(&req);
+    bereq.set_path(format!("/{}/404.html", BUCKET_NAME).as_str());
     beresp = bereq.send(BACKEND_NAME)?;
   }
 
   return Ok(beresp);
+}
+
+fn copy_request(req: &Request) -> Request {
+  let mut new = Request::new(req.get_method(), req.get_url());
+  req.get_header_names().for_each(|h| new.set_header(h, req.get_header(h).unwrap()));
+  return new;
 }
