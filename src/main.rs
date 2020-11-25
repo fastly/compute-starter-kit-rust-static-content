6d16425f7d65e0195c9c555bc30f21ef899b29af
Kailan Blanks <kblanks@fastly.com>
2020-11-25 12:58:37 +0000

    More header work

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! Compute@Edge static content starter kit program.
 
+use fastly::{Error, http::HeaderValue, Request, Response};
 use fastly::http::StatusCode;
-use fastly::{Error, Request, Response};
 
 /// The name of a backend server associated with this service.
 ///
@@ -14,6 +14,8 @@ const BUCKET_NAME: &str = "betts-gcp-gcs-fastly-tutorial";
 
 /// The host that the bucket is served on. This is used to make requests to the backend.
 const BUCKET_HOST: &str = "storage.googleapis.com";
+
+const ALLOWED_HEADERS: [&str; 3] = ["content-length", "content-type", "date"];
 
 /// The entry point for your application.
 ///
@@ -33,11 +35,13 @@ fn main(mut req: Request) -> Result<Response, Error> {
     // Otherwise just prepend the bucket name.
     format!("/{}{}", BUCKET_NAME, original_path)
   };
-
   req.set_path(&path);
 
   // Set the `Host` header to the bucket host rather than our C@E endpoint.
   req.set_header("Host", BUCKET_HOST);
+
+  // Used later to generate CORS headers.
+  let origin = req.get_header("origin");
 
   // Copy the modified client request to create a backend request.
   let mut bereq = copy_request(&req);
@@ -64,6 +68,39 @@ fn main(mut req: Request) -> Result<Response, Error> {
     // Send the request to the backend.
     beresp = bereq.send(BACKEND_NAME)?;
   }
+
+  // Remove extraneous headers from the backend response.
+  let mut to_remove: Vec<String> = Vec::new();
+  beresp.get_header_names().map(|n| n.to_string()).for_each(|name| {
+    if !ALLOWED_HEADERS.contains(&name.to_lowercase().as_str()) {
+      to_remove.push(name);
+    }
+  });
+  to_remove.iter().for_each(|h| {
+    beresp.remove_header(h);
+  });
+
+  // Apply referrer-policy and HSTS to HTML pages
+  if let Some(header) = beresp.get_header("content-type") {
+    if header.to_str().unwrap().starts_with("text/html") {
+      beresp.set_header(
+        "referrer-policy",
+        "origin-when-cross-origin",
+      );
+      beresp.set_header(
+          "strict-transport-security",
+          "dmax-age=3600",
+      );
+    }
+  }
+
+  // Apply Access-Control-Allow-Origin to allow cross-origin resource sharing
+  // Usually you would want a whitelist of domains here, but this example allows any origin to make requests.
+  let allowed_origins = match origin {
+    Some(val) => val.clone(),
+    _ => HeaderValue::from_str("*").unwrap(),
+  };
+  beresp.set_header("access-control-allow-origin", allowed_origins);
 
   // Return the backend response to the client.
   return Ok(beresp);
